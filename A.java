20889fbc25607079ce4f3956729f791b0990f6f6
Guri2001 <58091103+Guri2001@users.noreply.github.com>
2022-04-06 20:43:23 +0100

    Update A.java

--- A.java
+++ A.java
@@ -2,9 +2,6 @@ import java.util.*;
 public class A{
     public static void main(String args[]){
 
-        
-
-
         Scanner obj = new Scanner(System.in);
 
         int testCases = obj.nextInt();
@@ -26,22 +23,7 @@ public class A{
             }
             else if(a == 0 && b == 0){
                 System.out.println(1);
-            }
-        
-
-
-            
-
+            }                 
         }
-
-
-
-
-        
-
-
-
     }
-
-
 }
